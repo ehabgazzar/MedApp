fcefeffec55c23e7c4d4dae33ec8f815b600a290
Ehab <ehab.elgazzar@algorithm.ae>
2018-07-24 13:31:27 +0200

    - add required libraries, dependencies - add NavDrawer

--- app/src/main/java/com/example/ehab/medapp/MainActivity.java
+++ app/src/main/java/com/example/ehab/medapp/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.ehab.medapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Drawer result;
+    private Toolbar toolbar;
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.side_nav_bar)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Name" + " " +"last name").withEmail("email").withIcon(R.mipmap.ic_launcher)
+                ).withSelectionListEnabledForSingleProfile(false)
+                .build();
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Orders");
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName("Requests");
+
+//            PrimaryDrawerItem item3 = new PrimaryDrawerItem().withIdentifier(3).withName("Settings");
+        PrimaryDrawerItem item4 = new PrimaryDrawerItem().withIdentifier(4).withName("Logout");
+
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        item1, item2, new DividerDrawerItem(),  item4
+
+                ) .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        switch (position){
+
+
+                            case 1:
+                                fragment = null;
+                            //    fragment = new OrdersFragment();
+                                if (fragment != null) {
+
+//                                    toolbar.setTitle("Orders");
+                                    FragmentTransaction fragmentTransaction =
+                                            getSupportFragmentManager().beginTransaction();
+                                    fragmentTransaction.replace(R.id.fragment_container, fragment);
+                                    fragmentTransaction.commit();
+                                    onBackPressed();
+                                }
+                                break;
+
+                            case 2:
+                                fragment = null;
+                         //       fragment = new RequestsFragment();
+                                if (fragment != null) {
+
+//                                    toolbar.setTitle("Requests");
+                                    FragmentTransaction fragmentTransaction =
+                                            getSupportFragmentManager().beginTransaction();
+                                    fragmentTransaction.replace(R.id.fragment_container, fragment);
+                                    fragmentTransaction.commit();
+                                    onBackPressed();
+                                }
+                                break;
+                            case 4:
+
+                                break;
+
+                        }
+                        return false;
+                    }
+                }).build();
     }
 }
